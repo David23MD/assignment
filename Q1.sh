986b9cc742f2129a5a616db749bcb57b6b35866c
David23MD <karenzimucyodavid@gmail.com>
2023-12-15 09:24:32 +0200

    Question 1

---
+++ Q1.sh
@@ -0,0 +1,12 @@
+#Question 1
+#!/bin/bash
+
+# Check if the number of arguments is correct
+if [ "$#" -ne 3 ]; then
+    echo "Error: Incorrect number of arguments. Please provide three numbers." > error.txt
+    exit 1 
+fi
+
+# Sort the numbers in ascending order
+Echo –e “$1\n$2\n$3”| sort -n > numbers.txt
+
